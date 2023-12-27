d1047dec11a96e020078ba360b3e57fbab0b1678
Jagan Rajagopal <jaganrajagopal@outlook.com>
2023-12-27 07:23:24 +0530

    added new project for azure appservice

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace azureappserviceclassdemo.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
